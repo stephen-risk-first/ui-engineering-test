4962abea8a9d38062bb2ee6be70f38d674f1b2b5
stephen-risk-first <stephen.marx@riskfirst.com>
2020-03-25 11:33:23 +0000

    Change App to be a functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import { ListContainer as UserList } from './features/users/ListContainer';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return <UserList />;
-}
-
-export default App;
+export const App: React.FC = () => <UserList />;
